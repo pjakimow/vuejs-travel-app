3857d8c4c112afe6b5de5aca9f6097e2fb763aa2
Patrycja Jakimow <Patrycja.Jakimow@iteratec.com>
2020-01-10 11:28:50 +0100

    Make pages dynamic and add parameter to url

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,74 @@
+<template>
+    <nav id="nav">
+        <p class="logo">The Travel App</p>
+        <ul class="nav-links">
+            <li class="links">
+                <router-link to="/">
+                    Home
+                </router-link>
+            </li>
+            <li v-for="destination in destinations"
+                :key="destination.name" class="links">
+                <router-link :to="{
+                    name: 'DestinationDetails',
+                    params: {id: destination.id}
+                }">
+                    {{ destination.name }}
+                </router-link>
+            </li>
+        </ul>
+<!--        <router-link to="/">Home</router-link>-->
+<!--        <router-link to="/brazil">Brazil</router-link>-->
+<!--        <router-link to="/hawaii">Hawaii</router-link>-->
+<!--        <router-link to="/panama">Panama</router-link>-->
+<!--        <router-link to="/jamaica">Jamaica</router-link>-->
+    </nav>
+</template>
+
+<script>
+    import store from "@/store";
+    export default {
+        name: "TheNavigation",
+        data (){
+            return {
+                destinations: store.destinations,
+                destinationId: this.$route.params.id
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #nav{
+        display: flex;
+        /*align-items: center;*/
+        /*padding: 30px;*/
+    }
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+        text-decoration: none;
+    }
+    #nav a.router-link-exact-active {
+        color: red;
+    }
+    #nav a.my-custom-active-link-class {
+        color: orange;
+    }
+    .nav-links {
+        display: flex;
+    }
+    .links {
+        padding-right: 20px;
+        list-style: none;
+    }
+    .links:hover{
+        text-decoration: underline;
+    }
+    .logo {
+        font-size: 20px;
+        color: dodgerblue;
+        font-weight: bold;
+    }
+
+</style>
